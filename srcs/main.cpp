4d442ea4e72ee220b1ff4edc3f4cd32b7f77e017
Robin DEL AGUILA <delaguilarobin5@gmail.com>
2023-02-21 14:14:12 +0100

    connectionDone

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rdel-agu <rdel-agu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 17:36:41 by rdel-agu          #+#    #+#             */
-/*   Updated: 2023/02/20 18:14:05 by rdel-agu         ###   ########.fr       */
+/*   Updated: 2023/02/21 14:13:39 by rdel-agu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,6 @@
 #define MAX_CLIENTS 128
 
 int		printErr( std::string str ) { std::cerr << REDHB << str << CRESET; return ( 1 ); }
-
-// struct	Client {
-
-// 	int			sock;
-// 	std::string	nickname;
-// 	bool		active;
-// };
 
 int	main( int argc, char **argv ) {
 
@@ -43,10 +36,13 @@ int	main( int argc, char **argv ) {
 	server_socket = socket( AF_INET, SOCK_STREAM, 0);
 	if ( server_socket < 0 )
 		return ( printErr( "Error creating socket" ) );
-
+		
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	server_address.sin_port = htons( port );
+	
+	// BINDING SOCKET AND LISTENING TO SOCKET CHECK
+
 	if ( bind(server_socket, ( struct sockaddr* ) &server_address, sizeof( server_address ) ) < 0) 
 		return ( printErr( "Error binding socket" ) );
 	if ( listen( server_socket, 10 ) < 0 )
@@ -54,78 +50,55 @@ int	main( int argc, char **argv ) {
 
 	// TREATMENT
 
-	// fd_set				read_fds;
-	// std::vector<Client> clients(128);
-	// for ( int i = 0; i < 128; i++ )
-	// 	clients[ i ].active = false;
-
-	int				num_open_fds = 0, current_client;
-	struct pollfd	pfds[MAX_CLIENTS];
-	std::vector<int> clients;
+	int					num_open_fds = 0, current_client;
+	struct pollfd		pfds[MAX_CLIENTS];
+	std::vector<int>	clients;
+	std::string			welcome = "Welcome to the IRC server!\n";
+	char				buffer[4096];
 
 	pfds[0].fd = server_socket;
 	pfds[0].events = POLLIN;
 	
 	while ( true ) {
 		
-		std::cout << "bonjour" << std::endl;
+		// std::cout << "bonjour" << std::endl;
 		int	poll_count = poll( pfds, num_open_fds + 1, -1);
-
 		if( poll_count == -1 )
 			return ( printErr( "Error creating Poll" ) );
-		
 		if (pfds[0].revents & POLLIN ) {
 
 			if ( ( current_client = accept( server_socket, ( struct sockaddr * ) &server_address, ( socklen_t * ) &addrlen ) ) < 0 )
 				return ( printErr( "Can't accept" ) ); 
 		}
+		
 		num_open_fds++;
 		clients.push_back( current_client );
+		pfds[num_open_fds].fd = current_client;
+		pfds[num_open_fds].events = POLLIN;
+		send(current_client, welcome.c_str(), welcome.size(), 0 );
 		
-		
-		// FD_ZERO( &read_fds );
-		
-		// FD_SET(server_socket, &read_fds );
-		// for (int i = 0; i < 128; i++) {
-		
-		// 	if ( clients[i].active )
-		// 		FD_SET( clients[i].sock, &read_fds );
-		// }
-		// if ( FD_ISSET(server_socket, &read_fds ) )
-		// {
-		// 	struct sockaddr_in client_address;
-		// 	socklen_t client_len = sizeof( client_address );
-		// 	int client_socket = accept( server_socket, ( struct sockaddr* ) &client_address, &client_len );
-		// 	if ( client_socket < 0 )
-		// 		return ( printErr( "Error accepting connection" ) );
-		// 	for ( int i = 0; i < 128; i++ )
-		// 	{
-		// 		if ( !clients[i].active )
-		// 		{
-		// 			clients[i].sock = client_socket;
-		// 			clients[i].active = true;
-		// 			break;
-		// 		}
-		// 	}
-		// }
-
-		// for (int i = 0; i < 128; i++) {
-		
-		// 	if ( FD_ISSET( clients[i].sock, &read_fds ) ) {
+		for ( int i = 1; i <= num_open_fds; i++ ) {
 			
-		// 		char	buffer[4096];
-		// 		int r = read(clients[i].sock, buffer, 4096);
-		// 		if ( r < 0 )
-		// 			std::cerr << "Error reading from client" << std::endl;
-		// 		else if ( r == 0 )
-		// 			std::cerr << "Client disconnected" << std::endl;
-		// 		else {
-				
-		// 			std::cout << buffer << std::endl;
-		// 			std::cout << "hello there" << std::endl;
-		// 		}
-		// 	}
-		// }
+			if ( pfds[i].revents & POLLIN ) {
+
+				int valread = read( pfds[i].fd, buffer, 1024 );
+                std::cout << buffer << std::endl;
+                if (valread == 0) {
+
+                    close( pfds[i].fd );
+                    clients.erase( clients.begin() + i - 1 );
+                    pfds[i] = pfds[num_open_fds];
+                    num_open_fds--;
+                } else {
+					
+                    for ( int j = 0; j < num_open_fds; j++ ) {
+                        if (clients[j] != pfds[i].fd) {
+                            send( clients[j], buffer, strlen(buffer), 0 );
+                        }
+                    }
+                }
+			}
+		}
 	}
 	return ( 0 );
 }
